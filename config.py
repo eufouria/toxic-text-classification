b1fa7ba7be547545c3e2c87373039183945fd3b8
eufouria <vietkhoa137@gmail.com>
2024-04-13 18:18:28 -0700

    update: GCS model

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class CONFIG:
     DEVICE = "cpu"
-    MODEL_PATH = os.path.join(os.path.dirname(__file__),\
-            "classifier_model/my_awesome_model/finetuned_bert_1m.pt")
     TOXIC_THRESHOLD = 0.45
+    BUCKET_NAME = 'toxic-model'
+    SOURCE_BLOB_NAME = 'finetuned_bert_1m.pt'
+    DESTINATION_FILE_NAME = os.path.join(os.path.dirname(__file__),\
+            "classifier_model/my_awesome_model/finetuned_bert_1m.pt")
