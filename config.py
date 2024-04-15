33c4b2cf231bc2b338db8fd3ca118558eb57fbbd
eufouria <vietkhoa137@gmail.com>
2024-04-14 19:46:34 -0700

    update: model path

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ class CONFIG:
     TOXIC_THRESHOLD = 0.45
     BUCKET_NAME = 'toxic-model'
     SOURCE_BLOB_NAME = 'finetuned_bert_1m.pt'
-    DESTINATION_FILE_NAME = os.path.join(os.path.dirname(__file__),\
-            "classifier_model/my_awesome_model/finetuned_bert_1m.pt")
+    DESTINATION_FILE_NAME = 'finetuned_bert_1m.pt'
